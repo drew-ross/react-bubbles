05157f5152ced138ffba04169667e1de93db8c95
Drew Ross <mail.drew.ross@gmail.com>
2020-07-24 09:05:21 -0700

    Create form to retreive token from server and redirect to /bubbles page.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const initialFormValues = {
+  username: '',
+  password: ''
+};
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const history = useHistory();
+
+  const handleChanges = e => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', trimmedFormValues())
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        history.push('/bubbles');
+    })
+      .catch(err => console.log(err))
+  };
+
+  const trimmedFormValues = () => {
+    return {
+      username: formValues.username.trim(),
+      password: formValues.password.trim()
+    };
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username</label>
+        <br />
+        <input
+          id='username'
+          name='username'
+          value={formValues.username}
+          onChange={handleChanges}
+        />
+        <br />
+        <label htmlFor='password'>Password</label>
+        <br />
+        <input
+          id='password'
+          name='password'
+          type='password'
+          value={formValues.password}
+          onChange={handleChanges}
+        />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
     </>
   );
 };
